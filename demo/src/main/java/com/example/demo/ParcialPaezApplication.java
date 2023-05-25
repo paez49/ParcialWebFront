17c88b2a924605b6bddc3c199a89dc423d04ae75
Juan Páez <jdpluc302@gmail.com>
2023-05-25 16:50:58 -0500

    Backend

---
+++ demo/src/main/java/com/example/demo/ParcialPaezApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialPaezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialPaezApplication.class, args);
+	}
+
+}
