2509fe922de29ffce39aa24cb56ccc4a0d7d506b
Juan Páez <jdpluc302@gmail.com>
2023-05-25 17:43:40 -0500

    Back borrado

--- demo(1)/demo/src/main/java/com/example/demo/repository/LibroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibroRepository extends JpaRepository<Libro, Long> {
-}
